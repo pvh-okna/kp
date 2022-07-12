0d9f83bb33b82babfefb3884a614c9e83d18c316
maria_akhramovich <ahramovich2003@gmail.com>
2022-07-12 17:01:59 +0300

    work

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$('.header_burger').click(function (event) {
+		$('.header_burger,.header_menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+
+	$('.slider').slick({
+		arrows: false,
+		dots: true
+	});
+
+
+
+
+});
